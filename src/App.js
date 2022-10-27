c7709f91d7100fff306da98b0d84c061e53357c0
0p3r4d0r <anderson.valenzuela@udea.edu.co>
2022-10-27 10:57:00 -0500

    cammbios

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React anderson
+          Learn React anderson nnnnnnnnnnnnnnnnnnsdsddsdsds
         </a>
       </header>
     </div>
